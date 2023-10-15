33f02350305956470d747016287388f0160cbbe1
ubuntu <ubuntu@localhost.localdomain>
2023-10-15 16:47:50 +0800

    init

---
+++ PRST/userApp/models.py
@@ -0,0 +1,109 @@
+from django.db import models
+from datetime import datetime
+
+# 定义支付方式的枚举类型
+PAYMENT_METHODS = (
+    ('Bank Transfer', 'Bank Transfer'),
+    ('mailed', 'mailed'),  
+)
+
+# 定义性别的枚举类型
+GENDERS = (
+    ('M', 'Male'),
+    ('F', 'Female'),
+    ('O', 'Other'),
+)
+
+# 定义Employee类
+class Employee(models.Model):
+    id = models.AutoField(primary_key=True)#ID: 员工的唯一标识符
+    name = models.CharField(max_length=100)#Name: 员工的全名
+    position = models.CharField(max_length=100)#Position: 员工在公司中的职位或角色
+    hour_or_month = models.BooleanField()#Hour_or_Month: 员工的工资计算方式 (Hourly or Monthly) True: Hourly, False: Monthly
+    hourly_rate = models.FloatField(null=True, blank=True)#Hourly_Rate: 适用于按小时计费的员工 (与原HourlyRate合并)
+    monthly_salary = models.FloatField(null=True, blank=True)#Monthly_Salary: 适用于月薪员工 (与原Salary合并，如果适用)
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHODS)#Payment_Method: 员工选择的支付方式
+    payment_address = models.CharField(max_length=200,blank=True)#Payment_Address: 与所选支付方式相关的额外信息
+    payment_cardID = models.CharField(max_length=20,blank=True)#Payment_CardID: 与所选支付方式相关的额外信息
+    username = models.CharField(max_length=50)#Username: 系统登录用户名
+    password = models.CharField(max_length=50)#Password: 系统登录密码 (加密)  # 注意：实际应用中需要加密
+    address = models.CharField(max_length=200)#Address: 联系地址
+    phone_number = models.CharField(max_length=15)#Phone_Number: 联系电话
+    birth_date = models.DateField()#Birth_Date: 出生日期
+    gender = models.CharField(max_length=1, choices=GENDERS)#Gender: 性别
+    hour_limit = models.FloatField(null=True, blank=True)#Hour_Limit: 工时限制
+    
+    # 时间卡和报告将在其他模型中作为外键引用
+    # timecards = models.ManyToManyField('Timecard')
+    # reports = models.ManyToManyField('Report')
+    
+    def login(self, username, password):
+        # 实现登录逻辑
+        pass
+
+    def logout(self):
+        # 实现登出逻辑
+        pass
+
+    def view_profile(self):
+        # 实现查看个人资料逻辑
+        return self.__dict__
+
+    def edit_profile(self, details):
+        # 实现编辑个人资料逻辑
+        for key, value in details.items():
+            setattr(self, key, value)
+        self.save()
+    # 更多方法...
+    def submit_timecard(timecard: Timecard):
+        # 实现提交时间卡逻辑
+        pass
+    def view_payslip(pay_period: str):
+        # 实现查看工资条逻辑
+        pass
+    def select_payment_method(method: str, details: dict):
+        # 实现选择支付方式逻辑
+        pass
+    def view_timecard(timecard_id: int):
+        # 实现查看时间卡逻辑
+        pass
+    def edit_timecard(timecard_id: int, work_hours: float, task_details: str):
+        # 实现编辑时间卡逻辑
+        pass
+    def create_employee_report(report_type: str, start_date: datetime, end_date: datetime, employee_names: list):
+        # 实现创建员工报告逻辑
+        pass
+    def save_report(report: Report, name: str, location: str):
+        # 实现保存报告逻辑
+        pass
+    def create_timecard(start_date: datetime, end_date: datetime):
+        # 实现创建时间卡逻辑
+        pass
+    def update_timecard(timecard_id: int, charge_number: int, hours_worked: dict):
+        # 实现更新时间卡逻辑
+        pass
+
+
+
+# 定义 CommissionedEmployee 类
+class CommissionedEmployee(Employee):
+    # 新增属性
+    purchase_orders = models.JSONField()  # 存储佣金员工负责的采购订单列表
+    commission_rate = models.FloatField()  # 员工的佣金率
+    
+    # 新增方法
+    def create_purchase_order(self, details: dict):
+        # 实现创建新的采购订单逻辑
+        pass
+
+    def update_purchase_order(self, order_id: int, updated_details: dict):
+        # 实现更新现有的采购订单信息逻辑
+        pass
+
+    def delete_purchase_order(self, order_id: int):
+        # 实现删除指定的采购订单逻辑
+        pass
+
+# ...
+
+# 注意：以上代码为示例，并未实际运行。在实际应用中，还需要考虑如何加密敏感信息，以及如何与其他模型和数据库进行交互。
